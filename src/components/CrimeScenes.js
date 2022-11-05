ddd4406b8cc5431b44edc82582ab114b90276db8
Karl <karl.lu@outlook.com>
2022-11-05 02:48:46 -0400

    added minor styling fixes, added function to randomly generate 5 unique numbers btwn 1-151 in an array

--- src/components/CrimeScenes.js
+++ src/components/CrimeScenes.js
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+
 export default function CrimeScenes({ username, location }) {
+    const [pokemon, setPokemon] = useState([]);
+
+    // this function creates an array of 5 unique pokemon
+    function randomPokemon() {
+        let tempArray = [];
+        while (tempArray.length < 5) {
+            let newNumber = Math.floor(Math.random() * 151 + 1);
+            function isNotUnique(element) {
+                return element === newNumber;
+            }
+            // when none of the existing array entries matches the random number push to array
+            (!tempArray.some(isNotUnique) && tempArray.push(newNumber))
+        }
+        setPokemon(tempArray);
+    }
+
     return (
         <div className="CrimeScenes card">
             <h2>Welcome to {location}, {username}</h2>
+            <button onClick={randomPokemon}>Generate Pokemon</button>
         </div>
     )
 }
--- src/components/Location.js
+++ src/components/Location.js
@@ -12,7 +12,7 @@ export default function Location({setLocation}) {
             <h2>Pick a Location</h2>
             <div className="Location-locationsContainer">
                 {locations.map((location) => (
-                    <div className="Location-locationCard" onClick={() => handleClick(location)}>
+                    <div key={location} className="Location-locationCard" onClick={() => handleClick(location)}>
                         <img src={door} className="Location-locationImg" alt={door} />
                         <div>{location}</div>
                     </div>
