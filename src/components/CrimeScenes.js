58d8a5fc4aa5c25ae9259e35bad9757f95c8cfa0
serenakang <serenakang77@gmail.com>
2022-11-05 12:54:17 -0400

    Added the objects of location with latitude and longtitude

--- src/components/CrimeScenes.js
+++ src/components/CrimeScenes.js
@@ -1,7 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function CrimeScenes({ username, location }) {
     const [pokemon, setPokemon] = useState([]);
+    const [selectedLocation, setSelectedLocation] = useState()
+    console.log(location);
+    // useEffect(() => {
+    //     axios
+    //       // .get(`https://pokeapi.co/api/v2/pokemon`, {
+    //       .get(`https://data.police.uk/api/crimes-street/all-crime`, {
+    //         params: {
+    //           // limit: 151,
+    //           // London
+    //           lat: location.lat,
+    //           lng: location.lng,
+    //         //   lat: "51.5072",
+    //         //   lng: "-0.1276",
+    //           // // Manchester
+    //           // lat: "53.4839",
+    //           // lng: "-2.2446",
+    //           // Liverpool
+    //           // lat: "53.4084",
+    //           // lng: "-2.9916",
+    //         },
+    //       })
+    //       .then(function (res) {
+    //         const filtered = res.data
+    //         console.log(filtered)
+    //       })
+    // },[])
 
     // this function creates an array of 5 unique pokemon
     function randomPokemon() {
--- src/components/Location.js
+++ src/components/Location.js
@@ -1,10 +1,27 @@
 import door from '../assets/images/door.svg';
 
 export default function Location({setLocation}) {
-    const locations = ["London", "Yorkshire", "Worcestshire"]
+    const locations = [
+        {
+            city: "London",
+            lat: "51.5072",
+            lng: "-0.1276"
+        },
+        {
+            city: "Yorkshire",
+            lat: "53.9191",
+            lng: "-1.0792"
+        },
+        {
+            city: "Worcestshire",
+            lat: "52.2545",
+            lng: "-2.2668"
+        }
+    ]
 
-    function handleClick(location) {
-        setLocation(location);
+    function handleClick(location, lat, lng) {
+        setLocation([location, lat, lng]);
+        console.log(location);
     }
 
     return (
@@ -12,9 +29,9 @@ export default function Location({setLocation}) {
             <h2>Pick a Location</h2>
             <div className="Location-locationsContainer">
                 {locations.map((location) => (
-                    <div key={location} className="Location-locationCard" onClick={() => handleClick(location)}>
+                    <div key={location.city} className="Location-locationCard" onClick={() => handleClick(location.city, location.lat, location.lng)}>
                         <img src={door} className="Location-locationImg" alt={door} />
-                        <div>{location}</div>
+                        <div>{location.city}</div>
                     </div>
                 ))}
             </div>
