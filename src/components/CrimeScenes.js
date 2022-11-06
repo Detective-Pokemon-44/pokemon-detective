6b6126b6e52ad21b67a37a8e8814eb4a288a81c5
Karl <karl.lu@outlook.com>
2022-11-06 16:54:21 -0500

    general cleanup & changed expression for uniformity

--- src/components/CrimeScenes.js
+++ src/components/CrimeScenes.js
@@ -1,19 +1,12 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import {isNotUnique, randNum} from '../utils/functions';
+import { isNotUnique, randNum } from '../utils/functions';
 
 export default function CrimeScenes({ username, location }) {
     const [selectedLocation, setSelectedLocation] = useState();
     const [crimeSceneArray, setCrimeSceneArray] = useState();
     const [pokemonURL, setPokemonURL] = useState([]);
     const [pokemon, setPokemon] = useState([]);
-
-    // this function filter the array with same category name with placeholder, and choose random object 
-    function filter(array, placeholder){
-        const filteredArray = array.filter(({category}) => category === placeholder)
-        return filteredArray[Math.floor(Math.random() * filteredArray.length)]
-    }
-
 
     useEffect(() => {
         axios
@@ -29,30 +22,25 @@ export default function CrimeScenes({ username, location }) {
                 let selectedCrimes = [];
                 // goes through res data to ID all unique crimes and push to array
                 for (let crime in res.data) {
-                    (!isNotUnique(crimeTypes, res.data[crime].category) && crimeTypes.push(res.data[crime].category))
+                    (!isNotUnique(crimeTypes, res.data[crime].category) && crimeTypes.push(res.data[crime].category));
                 }
-
                 // creates an array of (up to) 5 random unique crimes depending on how many unique crimes in prev array
-                while(randomCrimes.length < (crimeTypes.length >= 5 ? 5 : crimeTypes.length)) {
+                while (randomCrimes.length < (crimeTypes.length >= 5 ? 5 : crimeTypes.length)) {
                     let num = randNum(crimeTypes.length, 0);
-                    (!isNotUnique(randomCrimes, crimeTypes[num]) && randomCrimes.push(crimeTypes[num]))
+                    (!isNotUnique(randomCrimes, crimeTypes[num]) && randomCrimes.push(crimeTypes[num]));
                 }
-
                 // for each unique crime in prev array, will select a random crime of the same category from the original res data
                 randomCrimes.forEach((crimeType) => {
                     let crimePool = [];
                     for (let crime in res.data) {
-                        if (res.data[crime].category === crimeType) {
-                            crimePool.push(res.data[crime]);
-                        }
+                        (res.data[crime].category === crimeType && crimePool.push(res.data[crime]));
                     }
                     selectedCrimes.push(crimePool[randNum(crimePool.length, 0)])
                 })
-                console.log(selectedCrimes);
                 // pushes 5 random unique crimes to state
                 setCrimeSceneArray(selectedCrimes);
             })
-        },[location])
+    }, [location])
 
     // this function creates an array of 5 unique pokemon
     function randomPokemon() {
