db3fa0354e5fbe514bce45a2560f249fa6f1cee5
pgm87to <philip.g.miles@gmail.com>
2022-11-06 23:01:18 -0500

    list select complete

--- src/components/CrimeScenes.js
+++ src/components/CrimeScenes.js
@@ -7,51 +7,60 @@ export default function CrimeScenes({ username, location }) {
   const [crimeSceneArray, setCrimeSceneArray] = useState();
   const [pokemonURL, setPokemonURL] = useState([]);
   const [pokemon, setPokemon] = useState([]);
+  const [crimeSelected, setCrimeSelected] = useState(null);
 
-    useEffect(() => {
-        axios
-            .get(`https://data.police.uk/api/crimes-street/all-crime`, {
-                params: {
-                    lat: location[1],
-                    lng: location[2],
-                },
-            })
-            .then(function (res) {
-                let crimeTypes = [];
-                let randomCrimes = [];
-                let selectedCrimes = [];
-                // goes through res data to ID all unique crimes and push to array
-                for (let crime in res.data) {
-                    (!isNotUnique(crimeTypes, res.data[crime].category) && crimeTypes.push(res.data[crime].category));
-                }
-                // creates an array of (up to) 5 random unique crimes depending on how many unique crimes in prev array
-                while (randomCrimes.length < (crimeTypes.length >= 5 ? 5 : crimeTypes.length)) {
-                    let num = randNum(crimeTypes.length, 0);
-                    (!isNotUnique(randomCrimes, crimeTypes[num]) && randomCrimes.push(crimeTypes[num]));
-                }
-                // for each unique crime in prev array, will select a random crime of the same category from the original res data
-                randomCrimes.forEach((crimeType) => {
-                    let crimePool = [];
-                    for (let crime in res.data) {
-                        (res.data[crime].category === crimeType && crimePool.push(res.data[crime]));
-                    }
-                    selectedCrimes.push(crimePool[randNum(crimePool.length, 0)])
-                })
-                // pushes 5 random unique crimes to state
-                setCrimeSceneArray(selectedCrimes);
-            })
-    }, [location])
 
-    // this function creates an array of 5 unique pokemon
-    function randomPokemon() {
-        let tempArray = [];
-        while (tempArray.length < 5) {
-            let newNumber = randNum(151, 1);
-            // when none of the existing array entries matches the random number push to array
-            (!isNotUnique(tempArray, newNumber) && tempArray.push(`https://pokeapi.co/api/v2/pokemon/${newNumber}`))
+
+
+  const handleCrimeClick = (crime) => {
+    setCrimeSelected(crime);
+
+  }
+
+  useEffect(() => {
+    axios
+      .get(`https://data.police.uk/api/crimes-street/all-crime`, {
+        params: {
+          lat: location[1],
+          lng: location[2],
+        },
+      })
+      .then(function (res) {
+        let crimeTypes = [];
+        let randomCrimes = [];
+        let selectedCrimes = [];
+        // goes through res data to ID all unique crimes and push to array
+        for (let crime in res.data) {
+          (!isNotUnique(crimeTypes, res.data[crime].category) && crimeTypes.push(res.data[crime].category));
         }
-        setPokemonURL(tempArray);
+        // creates an array of (up to) 5 random unique crimes depending on how many unique crimes in prev array
+        while (randomCrimes.length < (crimeTypes.length >= 5 ? 5 : crimeTypes.length)) {
+          let num = randNum(crimeTypes.length, 0);
+          (!isNotUnique(randomCrimes, crimeTypes[num]) && randomCrimes.push(crimeTypes[num]));
+        }
+        // for each unique crime in prev array, will select a random crime of the same category from the original res data
+        randomCrimes.forEach((crimeType) => {
+          let crimePool = [];
+          for (let crime in res.data) {
+            (res.data[crime].category === crimeType && crimePool.push(res.data[crime]));
+          }
+          selectedCrimes.push(crimePool[randNum(crimePool.length, 0)])
+        })
+        // pushes 5 random unique crimes to state
+        setCrimeSceneArray(selectedCrimes);
+      })
+  }, [location])
+
+  // this function creates an array of 5 unique pokemon
+  function randomPokemon() {
+    let tempArray = [];
+    while (tempArray.length < 5) {
+      let newNumber = randNum(151, 1);
+      // when none of the existing array entries matches the random number push to array
+      (!isNotUnique(tempArray, newNumber) && tempArray.push(`https://pokeapi.co/api/v2/pokemon/${newNumber}`))
     }
+    setPokemonURL(tempArray);
+  }
 
   // Function to convert the Pokemon URLs array into new array by making API call to access each URLs and get Pokemon Name and Type
   useEffect(() => {
@@ -75,36 +84,39 @@ export default function CrimeScenes({ username, location }) {
       {crimeSceneArray && (
         <ul className="CrimeScenes-category">
           {crimeSceneArray.map((individual) => {
-            return <li key={individual.id}>{individual.category}</li>;
+            return <li key={individual.id} onClick={!crimeSelected ? (e) => { handleCrimeClick(individual) } : null} > {individual.category}</li>;
           })}
         </ul>
-      )}
+      )
+      }
       {/* <button onClick={randomPokemon}>Generate Pokemon</button> */}
       <button onClick={randomPokemon}>Generate Pokemon</button>
 
-      {pokemon && (
-        <ul>
-          {pokemon.map((pokemonInfo) => {
-            return (
-                <li key={pokemonInfo.id}> 
-                    <p>
-                        {pokemonInfo.name} 
-                    </p>
-                    
-                    {pokemonInfo.types.map((pokemonPower) => {
-                        return (
-                            <p>
-                                {pokemonPower.type.name}
-                            </p>
-                        )
-                    }
-                    )}
-                 
+      {
+        pokemon && (
+          <ul>
+            {pokemon.map((pokemonInfo) => {
+              return (
+                <li key={pokemonInfo.id} >
+                  <p>
+                    {pokemonInfo.name}
+                  </p>
+
+                  {pokemonInfo.types.map((pokemonPower) => {
+                    return (
+                      <p>
+                        {pokemonPower.type.name}
+                      </p>
+                    )
+                  }
+                  )}
+
                 </li>
-            )
-          })}
-        </ul>
-      )}
-    </div>
+              )
+            })}
+          </ul>
+        )
+      }
+    </div >
   );
 }
