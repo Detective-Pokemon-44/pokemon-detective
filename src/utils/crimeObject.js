bbb17e2e154c9c67f6a7a657928aaa67b4a88170
serenakang <serenakang77@gmail.com>
2022-11-06 22:27:42 -0500

    Added arrayOfCrimeType variable with types of pokemons

---
+++ src/utils/crimeObject.js
@@ -0,0 +1,18 @@
+const arrayOfCrimeType = [
+  { antiSocialBehaviour: ["Grass", "Water", "Ice"] },
+  { bicyclTheft: ["Electric", "Bug", "Ice"] },
+  { burglary: ["Dark", "Ghost", "Rock"] },
+  { criminalDamageArson: ["Fire", "Ground", "Steel"] },
+  { drugs: ["Ground", "Water", "Flying"] },
+  { otherTheft: ["Poison", "Electric", "Grass"] },
+  { possessionOfWeapons: ["Psychic", "Rock", "Fighting"] },
+  { publicOrder: ["Water", "Normal", "Bug"] },
+  { robbery: ["Ghost", "Dark", "Flying"] },
+  { shoplifting: ["Normal", "Steel", "Poison"] },
+  { theftFromThePerson: ["Electric", "Bug", "Ice"] },
+  { vehicleCrime: ["Electric", "Bug", "Fire"] },
+  { violentCrime: ["Psychic", "Fighting", "Dragon"] },
+  { otherCrime: ["Fire", "Normal", "Grass"] },
+]
+
+export default arrayOfCrimeType
