8fb021d79bea2300b98ec26a34258cbc3e4f651d
Karl <karl.lu@outlook.com>
2022-11-06 22:11:09 -0500

    fixed issue w/keys in pokemon types map

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,7 +2,7 @@ export default function Pokemon({ pokemonInfo }) {
     return (
         <>
             <p><strong>{pokemonInfo.name}</strong></p>
-            {pokemonInfo.types.map((pokemonPower) => <p key={pokemonPower.id}>{pokemonPower.type.name}</p>
+            {pokemonInfo.types.map((pokemonPower) => <p key={pokemonPower.type.name}>{pokemonPower.type.name}</p>
             )}
         </>
     )
