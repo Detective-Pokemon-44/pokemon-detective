5275e0530f3bc8adfbd7ec355ba9f49dc97096c6
Karl <karl.lu@outlook.com>
2022-11-08 20:03:28 -0500

    added react modal to Pokemon component with onClick function, includes basic info & styling

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,12 +1,13 @@
 import ReactModal from 'react-modal';
 import useToggleState from '../hooks/useToggleState';
+import { capitalizeFirstLetter } from '../utils/functions';
 
 export default function Pokemon({ pokemonInfo }) {
     const [modalState, toggleModal] = useToggleState();
     return (
         <>
             <img className="Pokemon-image" src={require(`../assets/svgPokemon/${pokemonInfo.id}.svg`)} onClick={toggleModal} alt="Pokemon's image" />
-            <p><strong>{pokemonInfo.name}</strong></p>
+            <p><strong>{capitalizeFirstLetter(pokemonInfo.name)}</strong></p>
             {pokemonInfo.types.map((pokemonPower) => <p key={pokemonPower.type.name}>{pokemonPower.type.name}</p>
             )}
 
@@ -15,7 +16,13 @@ export default function Pokemon({ pokemonInfo }) {
                 className="Pokemon-modal"
                 onRequestClose={toggleModal}
             >
-                <p>I am {pokemonInfo.name}</p>
+                <img className="Pokemon-modalImage" src={require(`../assets/svgPokemon/${pokemonInfo.id}.svg`)} alt="Pokemon's image" />
+                <h3>{capitalizeFirstLetter(pokemonInfo.name)}</h3>
+                <div className="Pokemon-modalTypes">
+                    {pokemonInfo.types.map((pokemonPower) => <p key={pokemonPower.type.name}>{pokemonPower.type.name.toUpperCase()}</p>
+                    )}
+                </div>
+                <p>Due to its types, {capitalizeFirstLetter(pokemonInfo.name)} is good at solving 'TYPE' crimes.</p>
             </ReactModal>
         </>
     )
