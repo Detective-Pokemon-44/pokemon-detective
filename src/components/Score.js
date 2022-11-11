58892add59dcd30874cbcde7b47cf1af5eb0a389
paulavivas <paulavivasteixeira@gmail.com>
2022-11-10 23:39:28 -0500

    removed old code

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,33 +1,16 @@
 import firebaseConfig from "../utils/firebase"
-import { getDatabase, push, ref, onValue, remove } from "firebase/database"
-import { Route, Routes, useNavigate } from "react-router-dom"
-import { useState, useEffect } from "react"
-import Scoreboard from "./Scoreboard"
+import { getDatabase, push, ref} from "firebase/database"
+import { useNavigate } from "react-router-dom"
 
 export default function Score({ username, score }) {
-
   const navigate = useNavigate();
 
-  const [scoreData, setScoreData] = useState([])
   function saveName(event) {
     event.preventDefault()
     const database = getDatabase(firebaseConfig)
     const dbRef = ref(database, `container`)
     push(dbRef, {Name: username, Score: score})
   }
-
-  useEffect(() => {
-    const database = getDatabase(firebaseConfig)
-    const dbRef = ref(database, `container`)
-    onValue(dbRef, (res) => {
-      const temp = [];
-      const data = res.val()
-      for(let key in data){
-        temp.push({key: key, nameScore: data[key]})
-      }
-      setScoreData(temp)
-    })
-  }, [])
 
   return (
     <>
