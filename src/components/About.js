6a70479793f01277ab0882f389eaeb37587289f8
paulavivas <paulavivasteixeira@gmail.com>
2022-11-11 14:19:33 -0500

    branch to create about page

---
+++ src/components/About.js
@@ -0,0 +1,24 @@
+export default function About () {
+    return (
+        <>
+        <section className="About-header">
+            <h2>About</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt nulla esse eos quas commodi accusamus delectus. Dicta molestias ipsum maiores necessitatibus in quo eaque deserunt, tempore dignissimos repudiandae vero, esse iusto doloribus. Rerum maxime porro consequuntur ducimus enim nemo voluptatem eligendi debitis molestiae odit quam quod id dolorum maiores libero possimus esse, nobis sapiente tempora! Quibusdam ipsum reprehenderit blanditiis laboriosam quas cupiditate quae neque omnis at eveniet! Tempora, ut animi?</p>
+        </section>
+
+        <section>
+            <h2>Meet the creators</h2>
+            
+            <div className="Creators-container">
+
+            
+
+
+            </div>
+        </section>
+        
+        </>
+        
+    )
+
+}
