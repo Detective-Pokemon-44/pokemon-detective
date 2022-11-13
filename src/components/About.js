718ffa50498d01d3a2258b3fd049507cc71b9a6c
paulavivas <paulavivasteixeira@gmail.com>
2022-11-13 01:38:35 -0500

    edited css

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,93 @@
 export default function About () {
     return (
-        <>
-        <section className="About-header">
-            <h2>About</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt nulla esse eos quas commodi accusamus delectus. Dicta molestias ipsum maiores necessitatibus in quo eaque deserunt, tempore dignissimos repudiandae vero, esse iusto doloribus. Rerum maxime porro consequuntur ducimus enim nemo voluptatem eligendi debitis molestiae odit quam quod id dolorum maiores libero possimus esse, nobis sapiente tempora! Quibusdam ipsum reprehenderit blanditiis laboriosam quas cupiditate quae neque omnis at eveniet! Tempora, ut animi?</p>
-        </section>
+        <main className="About-page">
+            <section className="About-header wrapper">
+                <div>
+                    <h2>About</h2>
+                </div>
+                <div className="About-description">
+                    <div className="description-container">
+                        <p>Detective Pokemon is a game in which the user will be invited to solve different types of crime in three cities in the UK and will count on smart and fearless Pokemons to help them to solve the mysteries. </p>
+                        <p>This project was created by graduates at Juno College of Technology - Cohort 44. </p>
+                    </div>
+                    <div>
+                        <img className='pikachu-detective' src={require(`../assets/images/pokemonDetective.jpg`)} alt="Pikachu detective" />
+                    </div>
+                </div>
+            </section>
 
-        <section>
-            <h2>Meet the creators</h2>
-            
-            <div className="Creators-container">
+            <section className="wrapper">
+                <h2>Meet the Detectives</h2>
+                
+                <div className="Creators-container">
 
-            
+                    <div className="creator1">
+                        <div className="badge-container">
+                            <div className="badge-description">
+                                <p>Detective</p>
+                                <p className="detective-name">Karl Lu</p>
+                                <div className="medias">
+                                    <p className="button Button-about"><a href="https://github.com/lunarpirate" target="_blank" rel="noopener noreferrer">Github</a></p>
+                                    <p className="button Button-about"><a href="https://www.linkedin.com/in/karl-lu/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+                                </div>
+                            </div>
+                            <div className="badge-photo">
+                                <img className='detective-photo' src={require(`../assets/images/pokemonDetective.jpg`)} alt="Pikachu detective" />
+                            </div>
+                        </div>
+                    </div>
 
+                    <div className="creator2">
+                        <div className="badge-container">
+                            <div className="badge-description">
+                                <p>Detective</p>
+                                <p className="detective-name">Paula Vivas Teixeira</p>
+                                <div className="medias">
+                                    <p className="button Button-about"><a href="https://github.com/paulavivasteixeira" target="_blank" rel="noopener noreferrer">Github</a></p>
+                                    <p className="button Button-about"><a href="https://www.linkedin.com/in/paulavivasteixeira/?locale=en_US" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+                                </div>
+                            </div>
+                            <div className="badge-photo">
+                                <img className='detective-photo' src={require(`../assets/images/paula.png`)} alt="Paula" />
+                            </div>
+                        </div>
+                    </div>
 
-            </div>
-        </section>
-        
-        </>
+                    <div className="creator3">                 
+                        <div className="badge-container">
+                            <div className="badge-description">
+                                <p>Detective</p>
+                                <p className="detective-name">Philip Miles</p>
+                                <div className="medias">
+                                    <p className="button Button-about"><a href="https://github.com/pgm87" target="_blank" rel="noopener noreferrer">Github</a></p>
+                                    <p className="button Button-about"><a href="https://www.linkedin.com/in/philip-miles-01570b24b/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+                                </div>
+                            </div>
+                            <div className="badge-photo">
+                            <img className='detective-photo' src={require(`../assets/images/pokemonDetective.jpg`)} alt="Pikachu detective" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="creator4">
+                        <div className="badge-container">
+                            <div className="badge-description">
+                                <p>Detective</p>
+                                <p className="detective-name">Serena Kang</p>
+                                <div className="medias">
+                                <p className="button  Button-about"><a href="https://github.com/serenakang77" target="_blank" rel="noopener noreferrer">Github</a></p>
+                                <p className="button Button-about"><a href="https://www.linkedin.com/in/serena-kang-0443941b2/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+                                </div>
+                            </div>
+                            <div className="badge-photo">
+                            <img className='detective-photo' src={require(`../assets/images/pokemonDetective.jpg`)} alt="Pikachu detective" />
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </section>   
+        </main>
         
     )
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom"
+
+
 export default function Navbar() {
     return (
         <>
             <header className="Navbar">
                 <h1>Detective Pokemon</h1>
+                <Link to='/about'>About</Link>
             </header>
         </>
     )
