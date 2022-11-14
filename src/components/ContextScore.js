4940984899d6e572c3cb747b501f6796b3344f8e
Karl <karl.lu@outlook.com>
2022-11-13 22:59:38 -0500

    added context with score

---
+++ src/components/ContextScore.js
@@ -0,0 +1,28 @@
+import React, { useState, useContext } from 'react';
+
+const ScoreContext = React.createContext();
+const ScoreUpdateContext = React.createContext();
+
+export function useScore() {
+    return useContext(ScoreContext);
+}
+
+export function useUpdateScore() {
+    return useContext(ScoreUpdateContext);
+}
+
+export function ScoreProvider({ children }) {
+    const [score, setScore] = useState();
+
+    function updateScore(amt) {
+        setScore(prevScore => prevScore + amt);
+    }
+
+    return (
+        <ScoreContext.Provider value={score}>
+            <ScoreUpdateContext.Provider value={updateScore}>
+                {children}
+            </ScoreUpdateContext.Provider>
+        </ScoreContext.Provider>
+    )
+}
