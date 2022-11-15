ab02b7fbc25bbe87a7f301d70b45f6c2af3bb729
Karl <karl.lu@outlook.com>
2022-11-14 20:42:23 -0500

    added Score component to Gamebar, edited styles to accommodate changes

--- src/components/Gamebar.js
+++ src/components/Gamebar.js
@@ -1,10 +1,11 @@
 import { useScore } from './ContextScore';
+import Score from './Score';
 
 export default function Gamebar() {
     const score = useScore();
     return (
         <div className="Gamebar">
-            <span>Quit</span>
+            <span><Score /></span>
             <span>{score > 0 && `Cases Solved: ${score}`}</span>
         </div>
     )
