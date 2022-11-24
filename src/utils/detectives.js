25bc19492e9e9a6ea22826ca6bdd96c6eb90dff1
serenakang <serenakang77@gmail.com>
2022-11-24 15:11:23 -0500

    Changed Serena linkedin url

--- src/utils/detectives.js
+++ src/utils/detectives.js
@@ -1,26 +1,26 @@
 export const detectives = [
-    {
-        name: "Karl Lu",
-        github: "https://github.com/lunarpirate",
-        linkedin: "https://www.linkedin.com/in/karl-lu",
-        image: "karl.jpg"
-    },
-    {
-        name: "Paula Vivas",
-        github: "https://github.com/paulavivasteixeira",
-        linkedin: "https://www.linkedin.com/in/paulavivasteixeira/?locale=en_US",
-        image: "paula.png"
-    },
-    {
-        name: "Phil Miles",
-        github: "https://github.com/pgm87",
-        linkedin: "https://www.linkedin.com/in/philip-miles-01570b24b/",
-        image: "phil.jpg"
-    },
-    {
-        name: "Serena Kang",
-        github: "https://github.com/serenakang77",
-        linkedin: "https://www.linkedin.com/in/serena-kang-0443941b2/",
-        image: "serena.webp"
-    }
-];
+  {
+    name: "Karl Lu",
+    github: "https://github.com/lunarpirate",
+    linkedin: "https://www.linkedin.com/in/karl-lu",
+    image: "karl.jpg",
+  },
+  {
+    name: "Paula Vivas",
+    github: "https://github.com/paulavivasteixeira",
+    linkedin: "https://www.linkedin.com/in/paulavivasteixeira/?locale=en_US",
+    image: "paula.png",
+  },
+  {
+    name: "Phil Miles",
+    github: "https://github.com/pgm87",
+    linkedin: "https://www.linkedin.com/in/philip-miles-01570b24b/",
+    image: "phil.jpg",
+  },
+  {
+    name: "Serena Kang",
+    github: "https://github.com/serenakang77",
+    linkedin: "https://www.linkedin.com/in/serena-kang-dev/",
+    image: "serena.webp",
+  },
+]
